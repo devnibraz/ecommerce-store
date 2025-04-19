7b6e98bee1c57d55cc4cf460495f179260634d3b
devnibraz <nibraz.dev@gmail.com>
2025-04-19 11:03:54 -0700

    Chnaged to Nibraz Dev

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Nibraz</h1>
+      <h1>Nibraz Dev</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
